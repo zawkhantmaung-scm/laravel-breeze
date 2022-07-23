ab14f768f0a5e233c60aa5dbf23bd07c7d2ac4c8
zawkhantmaung-scm <zaw.khantmaung@seattleconsultingmyanmar.com>
2022-07-24 00:53:06 +0630

    composer require livewire/livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col align-middle items-center">
+    <h1>{{ $count }}</h1>
+    <button class="w-40 h-40 bg-blue-800 hover:bg-blue-900 transition duration-150 ease-in-out" wire:click="increment" />
+</div>
